a49c356b2ffb9358b3de73718bacd94ac3c4f0bc
Matheus Rodrigues <84551135+Httpstheus@users.noreply.github.com>
2023-03-09 21:59:56 -0300

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>
+  <h1>Olá Vue!</h1>
+  <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+ </div>
+</template>
+
+<script>
+ 
+export default {
+ name: 'HelloWorld',
+ data(){
+    return {
+      nome: "Matheus",
+      profissao: "Programador"
+    }
+ }
+}
+</script>
