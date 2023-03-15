011b56c48f5c5eba67c90cfe5b81ad59548ab3f1
Matheus Rodrigues <84551135+Httpstheus@users.noreply.github.com>
2023-03-14 22:48:17 -0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,43 @@
+ <template>
+  <div>
+    <Header/>
+    <HelloWorld/>
+    <LifeCycle/>
+    <Pessoa/>
+  </div>
+ </template>
+
+ <script>
+ import Header from './components/09 - Header.vue'
+ import HelloWorld from './components/01 - HelloWorld.vue'
+ import LifeCycle from './components/02 - LifeCycle.vue'
+ import Pessoa from './components/03 - Pessoa.vue'
+
+
+ export default {
+  name: 'App',
+  components: {
+    Header,
+    HelloWorld,
+    LifeCycle,
+    Pessoa
+
+  }
+ }
+</script>
+
+<style>
+  body{
+    background: #333;
+    color: #fff;
+  }
+
+  a {
+    color: red;
+  }
+
+  .p-text {
+    color: greenyellow;
+  }
+
+</style>
