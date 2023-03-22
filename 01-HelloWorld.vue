f4b415588e83dcaea8d53dfac109a3a21f9bdff5
Matheus Rodrigues <84551135+Httpstheus@users.noreply.github.com>
2023-03-22 20:10:34 -0300

    Update 01-HelloWorld.vue

--- 01-HelloWorld.vue
+++ 01-HelloWorld.vue
@@ -2,18 +2,26 @@
  <div>
   <h1>Olá Vue!</h1>
   <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+  <MultiplosEventos/>
  </div>
 </template>
 
 <script>
  
+  import MultiplosEventos from './MultiplosEventos';
+
 export default {
- name: 'HelloWorld',
- data(){
-    return {
-      nome: "Matheus",
-      profissao: "Programador"
-    }
+
+
+    name: 'HelloWorld',
+    components: {
+      MultiplosEventos
+    },
+    data(){
+        return {
+          nome: "Matheus",
+          profissao: "Programador"
+        }
  }
 }
-</script>
+</script>  
